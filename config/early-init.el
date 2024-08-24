d943e0857e8cb26f73ddaab347d958672decb413
Erik Sundin <mail@eriksundin.com>
2024-08-24 23:31:16 +0200

    initial setup

---
+++ config/early-init.el
@@ -0,0 +1,27 @@
+;;; early-init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 erik
+;;
+;; Author: erik <erik@nixos>
+;; Maintainer: erik <erik@nixos>
+;; Created: augusti 24, 2024
+;; Modified: augusti 24, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/erik/early-init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(setq package-enable-at-startup nil)
+
+
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A flake with Emacs as the default package for x86_64-linux";
+
+  inputs = {
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    dotfiles = {
+      url = "github:erik-sundin-git/dotfiles";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    dotfiles,
+    emacs-overlay,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        system = system;
+        overlays = [emacs-overlay.overlays.emacs emacs-overlay.overlays.package];
+      };
+    in {
+
+      # Set Emacs as the default package
+      defaultPackage = pkgs.emacsWithPackagesFromUsePackage {
+        config = "./config/init.el";
+        package = pkgs.emacs-git;
+      };
+
+      # Define the defaultApp to run Emacs with the custom init file
+      defaultApp = {
+        type = "app";
+        program = "${self.defaultPackage}/bin/emacs";
+      };
+
+      # Set up a development shell with Emacs
+      devShell = pkgs.mkShell {
+        buildInputs = [pkgs.custom-emacs];
+      };
+    });
+}
